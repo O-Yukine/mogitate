e1303637159c275679b8a9f1174260f9fa9f4f5d
Yukine <yukineriko@gmail.com>
2025-10-13 08:07:36 +0900

    flex wrapの適用

--- src/resources/views/products.blade.php
+++ src/resources/views/products.blade.php
@@ -11,8 +11,8 @@
             <a class="product-contents__register" href="{{ asset('/products/register') }}">+商品を追加</a>
         </div>
         <div class="product-contents__inner">
-            <form class="form" action="/products/search" method="get">
-                <div class="product-contents__search">
+            <div class="product-contents__search">
+                <form class="form" action="/products/search" method="get">
                     <div class="product-contents__search--list">
                         <ul>
                             <li><input class="product-contents__search--keyword" type="text" placeholder="商品名で検索"
@@ -21,27 +21,33 @@
                             <li>価格順で表示</li>
                             <li><select class="product-contents__sort" name="" id="">
                                     <option value="">価格で並べ替え</option>
+                                    <option value="price_asc" {{ request('sort') == 'price_asc' ? 'selected' : '' }}>価格が安い順
+                                    </option>
+                                    <option value="price_desc" {{ request('sort') == 'price_desc' ? 'selected' : '' }}>
+                                        価格が高い順</option>
                                 </select></li>
                         </ul>
                     </div>
-                </div>
-            </form>
-            @foreach ($products as $product)
-                <a href="{{ url('/products/' . $product->id) }}">
-                    <div class="card">
-                        <div class="card__img">
-                            <img src="{{ asset('storage/images/' . $product->image) }}" alt="画像" />
-                        </div>
-                        <div class="card__content">
-                            <div class="card__content-tag">
-                                <p class="card__content-tag-item"> {{ $product->name }} </p>
-                                <p class="card__content-tag-item card__content-tag-item--last"> {{ $product->price }}
-                                </p>
+                </form>
+            </div>
+            <div class="product-contents__products">
+                @foreach ($products as $product)
+                    <a href="{{ url('/products/' . $product->id) }}">
+                        <div class="card">
+                            <div class="card__img">
+                                <img src="{{ asset('storage/images/' . $product->image) }}" alt="画像" />
+                            </div>
+                            <div class="card__content">
+                                <div class="card__content-tag">
+                                    <p class="card__content-tag-item"> {{ $product->name }} </p>
+                                    <p class="card__content-tag-item card__content-tag-item--last"> {{ $product->price }}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-            @endforeach
+                    </a>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection

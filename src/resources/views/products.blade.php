3dbfabeea891918c7e5e34cc298e5a00d974880b
Yukine <yukineriko@gmail.com>
2025-10-15 06:40:48 +0900

    最終確認

--- src/resources/views/products.blade.php
+++ src/resources/views/products.blade.php
@@ -39,13 +39,15 @@
                 @foreach ($products as $product)
                     <a href="{{ url('/products/' . $product->id) }}">
                         <div class="card">
+
                             <div class="card__img">
                                 <img src="{{ asset('storage/images/' . $product->image) }}" alt="画像" />
                             </div>
                             <div class="card__content">
                                 <div class="card__content-tag">
                                     <p class="card__content-tag-item"> {{ $product->name }} </p>
-                                    <p class="card__content-tag-item card__content-tag-item--last"> {{ $product->price }}
+                                    <p class="card__content-tag-item card__content-tag-item--last">
+                                        {{ '¥' . $product->price }}
                                     </p>
                                 </div>
                             </div>
